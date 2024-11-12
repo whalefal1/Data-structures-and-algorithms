a1ac4643cac3d55376a87f34994b6a7698e8ba08
whalefal1 <450582885@qq.com>
2024-11-12 20:03:06 +0800

    长度最小的子数组

---
+++ src/com.dataStructuresAndAlgorithms/java/Arrays/MinSubArrayLen.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class MinSubArrayLen {
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        int start = 0, end = 0;
+        int sum = 0;
+        while (end < n) {
+            sum += nums[end];
+            while (sum >= target) {
+                ans = Math.min(ans, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+}
+
