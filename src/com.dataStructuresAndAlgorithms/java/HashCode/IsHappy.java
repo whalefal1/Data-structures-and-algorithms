6e6a336ee3337c3e79c7d18b4620d73158ae23c7
whalefal1 <450582885@qq.com>
2024-11-23 10:07:04 +0800

    快乐数

---
+++ src/com.dataStructuresAndAlgorithms/java/HashCode/IsHappy.java
@@ -0,0 +1,28 @@
+package HashCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class IsHappy {
+
+    //获取每位平方和
+    private int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    //使用Set判断是否存在环形链
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+}
