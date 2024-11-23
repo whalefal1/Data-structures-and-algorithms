d609f241da1095b2cbacc6b811a0a2f79c095246
whalefal1 <450582885@qq.com>
2024-11-23 22:07:06 +0800

    汇总区间

---
+++ src/com.dataStructuresAndAlgorithms/java/util/sort/QuickSort.java
@@ -0,0 +1,28 @@
+package util.sort;
+
+public class QuickSort {
+    //快速排序
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int index = getIndex(arr, low, high);
+            quickSort(arr, low, index - 1);
+            quickSort(arr, index + 1, high);
+        }
+    }
+
+    public static int getIndex(int[] arr, int low, int high) {
+        int temp = arr[low];
+        while (low < high) {
+            while (low < high && arr[high] >= temp) {
+                high--;
+            }
+            arr[low] = arr[high];
+            while (low < high && arr[low] <= temp) {
+                low++;
+            }
+            arr[high] = arr[low];
+        }
+        arr[low] = temp;
+        return low;
+    }
+}
