cf64da4c7eb90c74ab37af5611480d4b40962bce
whalefal1 <450582885@qq.com>
2024-11-23 10:19:45 +0800

    重复元素||

---
+++ src/com.dataStructuresAndAlgorithms/java/HashCode/ContainsNearbyDuplicate.java
@@ -0,0 +1,20 @@
+package HashCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsNearbyDuplicate {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            int num = nums[i];
+            if (map.containsKey(num) && i - map.get(num) <= k) {
+                return true;
+            }
+            map.put(num, i);
+        }
+        return false;
+    }
+}
