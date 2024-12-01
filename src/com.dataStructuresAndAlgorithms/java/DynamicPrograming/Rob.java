33eee34e655d6e5b012214a6b47bdf1c14f62322
Li Pengxun <450582885@qq.com>
2024-12-01 16:32:44 +0800

    图的中心节点——找到任意两条边的公共节点

---
+++ src/com.dataStructuresAndAlgorithms/java/DynamicPrograming/Rob.java
@@ -0,0 +1,26 @@
+package DynamicPrograming;
+
+public class Rob {
+
+    public static int rob  (int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        // 子问题：
+        // f(k) = 偷 [0..k) 房间中的最大金额
+
+        // f(0) = 0
+        // f(1) = nums[0]
+        // f(k) = max{ rob(k-1), nums[k-1] + rob(k-2) }
+
+        int N = nums.length;
+        int[] dp = new int[N+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int k = 2; k <= N; k++) {
+            dp[k] = Math.max(dp[k-1], nums[k-1] + dp[k-2]);
+        }
+        return dp[N];
+    }
+
+}
