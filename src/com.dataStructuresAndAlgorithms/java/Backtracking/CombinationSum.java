d5f8a1b15f1c7fa8defebdfcac97653d7eba7959
Li Pengxun <450582885@qq.com>
2024-12-05 15:29:35 +0800

    组合总和

---
+++ src/com.dataStructuresAndAlgorithms/java/Backtracking/CombinationSum.java
@@ -0,0 +1,36 @@
+package Backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> combine = new ArrayList<Integer>();
+        dfs(candidates, target, ans, combine, 0);
+        return ans;
+    }
+
+
+
+    //candidates：原数组 target：目标值 ans：结果集 combine：一个结果 idx：当前遍历到的节点
+    public void dfs(int[] candidates, int target, List<List<Integer>> ans, List<Integer> combine, int idx) {
+        //递归三要素：返回值、递归结束条件、递归式
+        if (idx == candidates.length) {
+            return;
+        }
+        if (target == 0) {
+            ans.add(new ArrayList<Integer>(combine));
+            return;
+        }
+        // 直接跳过
+        dfs(candidates, target, ans, combine, idx + 1);
+        // 选择当前数
+        if (target - candidates[idx] >= 0) {
+            combine.add(candidates[idx]);
+            dfs(candidates, target - candidates[idx], ans, combine, idx);
+            combine.remove(combine.size() - 1);
+        }
+    }
+}
