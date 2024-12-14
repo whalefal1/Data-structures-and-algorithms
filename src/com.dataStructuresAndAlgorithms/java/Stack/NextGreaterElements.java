5b8d65e6e9d6d8271b5fca0a1e5e323cd2db6fc5
Li Pengxun <450582885@qq.com>
2024-12-14 10:22:17 +0800

    单调栈：下一个更大元素 II

---
+++ src/com.dataStructuresAndAlgorithms/java/Stack/NextGreaterElements.java
@@ -0,0 +1,27 @@
+package Stack;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+public class NextGreaterElements {
+
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        Deque<Integer> st = new ArrayDeque<>();
+        for (int i = 0; i < 2 * n; i++) {
+            int x = nums[i % n];
+            while (!st.isEmpty() && x > nums[st.peek()]) {
+                // x 是 nums[st.peek()] 的下一个更大元素
+                // 既然 nums[st.peek()] 已经算出答案，则从栈顶弹出
+                ans[st.pop()] = x;
+            }
+            if (i < n) {
+                st.push(i);
+            }
+        }
+        return ans;
+    }
+}
